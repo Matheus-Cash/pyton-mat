bb352d78f717144b618f1839c40737c8999aa883
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-10 18:00:28 +0000

    14

---
+++ python.py
@@ -0,0 +1,14 @@
+impor math
+
+## -b +- Raiz (b2 - 4 * a * C) 2/a
+
+a = float(input('digite o valor de a:'))
+b = ......
+
+delta = (b ** 2) - (4 * a * c)
+
+
+x1 = (-b + Raiz quadrada de delta) / (2*a)
+x2 =-(b - Raiz quadrada de delta) / (2*a)
+
+print(f'O x1 é {x1} e o x2 {x2}')
