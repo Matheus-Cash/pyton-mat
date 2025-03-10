f9430e62c617cd4cddf2dbf82e648b6f49343d8a
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-10 18:04:54 +0000

    1234

--- python.py
+++ python.py
@@ -11,4 +11,4 @@ delta = (b ** 2) - (4 * a * c)
 x1 = (-b + Raiz quadrada de delta) / (2*a)
 x2 =-(b - Raiz quadrada de delta) / (2*a)
 
-print(f'O x1 é {x1} e o x2 {x2}')
+print(f'O x1 é {x1} e o x2 é o {x2}')
