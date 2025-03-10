66eeb776ac664631bf71a8f62fb33555eb05bcd1
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-10 18:03:34 +0000

    124

--- python.py
+++ python.py
@@ -1,14 +1,17 @@
-impor math
+import math 
 
-## -b +- Raiz (b2 - 4 * a * C) 2/a
 
-a = float(input('digite o valor de a:'))
-b = ......
+a = float(input('Digite o valor de a: '))
+b = float(input('Digite o valor de b: '))
+c = float(input('Digite o valor de c: '))
+
 
 delta = (b ** 2) - (4 * a * c)
 
+if delta >= 0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
 
-x1 = (-b + Raiz quadrada de delta) / (2*a)
-x2 =-(b - Raiz quadrada de delta) / (2*a)
-
-print(f'O x1 é {x1} e o x2 {x2}')
+    print(f'O x1 é {x1} e o x2 é {x2}')
+else:
+    print('Delta é negativo, não existem raízes reais.')
