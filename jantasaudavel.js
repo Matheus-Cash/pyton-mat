734563cad499ffb3a69418e79c9218698efa81b9
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-12 18:11:15 +0000

    124

--- jantasaudavel.js
+++ jantasaudavel.js
@@ -1,15 +1,9 @@
 const jantaSaudavel = {
-carboidrato: 'Batata Doce Assada',
-salada: 'Alface com Azeite de Oliva',
-proteina: 'Peito de Frango Grelhado',
-bebida: 'Suco de Laranja Natural',
-sobremesa: 'Salada de Frutas',
-
-objjantasaudavel.carboidrato = 'Batata Doce Assada';
-objjantasaudavel.salada = 'Alface com Azeite de Oliva';
-objjantasaudavel.proteina = 'Peito de Frango Grelhado';
-objjantasaudavel.bebida = 'Suco de Laranja Natural';
-objjantasaudavel.sobremesa = 'Salada de Frutas';
+    carboidrato: 'Batata Doce Assada',
+    salada: 'Alface com Azeite de Oliva',
+    proteina: 'Peito de Frango Grelhado',
+    bebida: 'Suco de Laranja Natural',
+    sobremesa: 'Salada de Frutas'
+};
 
 console.log(jantaSaudavel);
-}
