677a82943adcd74c88ac49bf091962e1364abde5
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-12 17:22:59 +0000

    134

---
+++ jantasaudavel.js
@@ -0,0 +1,13 @@
+carboidrato: 'Batata Doce Assada',
+salada: 'Alface com Azeite de Oliva',
+proteina: 'Peito de Frango Grelhado',
+bebida: 'Suco de Laranja Natural',
+sobremesa: 'Salada de Frutas',
+
+objjantasaudavel.carboidrato = 'Batata Doce Assada';
+objjantasaudavel.salada = 'Alface com Azeite de Oliva';
+objjantasaudavel.proteina = 'Peito de Frango Grelhado';
+objjantasaudavel.bebida = 'Suco de Laranja Natural';
+objjantasaudavel.sobremesa = 'Salada de Frutas';
+
+console.log(objjantasaudavel);
---
+++ objetos.js
@@ -0,0 +1,9 @@
+const objetopessoa = {
+    nome: 'Matheus',
+    idade: '16',
+    profissao: '244',
+};
+
+objetopessoa.idade = '17';
+
+console.log(objetopessoa)
