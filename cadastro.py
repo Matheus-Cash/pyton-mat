7fa528b5f8485f050c544942affbc7bc4b1e8de5
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-13 17:31:24 +0000

    134

--- cadastro.py
+++ cadastro.py
@@ -6,14 +6,13 @@ while True:
     print('3. Sair')
     opcao = input('Escolha uma opção: ')
     if opcao == '1':
-    print(''\n--- Adicionar Cliente ---'')
-    nome = input('Nome: ')
-    telefone = input ('telefone')
-
-    cliente = {
-        'nome': nome,
-        'telefone': telefone,
-    }
-
-    clientes.append(cliente)
-    print(''\n cliente cadastrado com sucesso'')
+        nome = input('Digite o nome do cliente: ')
+        clientes.append(nome)
+    elif opcao == '2':
+        print('Clientes cadastrados:')
+        for cliente in clientes:
+            print(cliente)
+    elif opcao == '3':
+        break
+    else:
+        print('Opção inválida, tente novamente.')
